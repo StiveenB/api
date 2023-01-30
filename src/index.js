db3aa3cd4a8bbbf615300aeae9234a4995c9226f
Stiveen Bolaños <qwerqwer123123@outlook.es>
2023-01-29 21:09:00 -0500

    mensaje

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { sequelize } from './database/database.js';
 
 
 async function main(){
-        await sequelize.sync({force: true})
+        //await sequelize.sync({force: true})
         console.log("Coenccion establecida");
         await app.listen(app.get('port'));
         console.log('server port', app.get('port'));
